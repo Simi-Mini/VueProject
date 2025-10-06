0cb3fa539dd98c29ea8b8366fc5a41e341ae071e
Simi-Mini <simon.rapp@student.htldornbirn.at>
2025-10-06 10:38:24 +0200

    erstes type scrpit file
    erstes typescript file

---
+++ Typescript/demo.ts
@@ -0,0 +1,11 @@
+let a: number = 7;
+let b: number = 3;
+
+function add(number1: number, number2: number): number {
+    return number1 + number2;
+}
+
+
+
+let result: number = add(4, 3);
+console.log(result);
